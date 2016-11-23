a422bc74e9d63223951b4c80b4674e1463706f5d
Jon Lieblich <jlieblich1@gmail.com>
2016-11-23 10:52:09 -0500

    fuckedup

--- GitBranchingDemo/app/src/main/java/com/scottlindley/gitbranchingdemo/MainActivity.java
+++ GitBranchingDemo/app/src/main/java/com/scottlindley/gitbranchingdemo/MainActivity.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+
+import java.util.ArrayList;
 import java.util.List;
 import android.support.v7.app.AppCompatActivity;
 
@@ -17,11 +19,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mActors = new ArrayList<>();
+        
         LinearLayoutManager manager = new LinearLayoutManager(getApplicationContext(), LinearLayoutManager.VERTICAL, false);
         mRecycler = (RecyclerView)findViewById(R.id.recyclerView);
         mAdapter = new GitStuffAdapter(mActors);
         mRecycler.setLayoutManager(manager);
         mRecycler.setAdapter(mAdapter);
 
+        Actor me = new Actor("Jon", "Yesterday", 2);
+        Actor Jay = new Actor("Jay", "Unknown, maybe the late 1700s", 0);
+        Actor Scott = new Actor("Scott", "At some point in time", -1);
+        mActors.add(me);
+        mActors.add(Jay);
+        mActors.add(Scott);
+        mAdapter.notifyDataSetChanged();
     }
 }
