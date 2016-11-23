258c9b7fbb2f4011cbede5933ba21223e60cb58d
Jon Lieblich <jlieblich1@gmail.com>
2016-11-23 11:18:25 -0500

    cleaning

--- GitBranchingDemo/app/src/main/java/com/scottlindley/gitbranchingdemo/MainActivity.java
+++ GitBranchingDemo/app/src/main/java/com/scottlindley/gitbranchingdemo/MainActivity.java
@@ -1,17 +1,12 @@
 package com.scottlindley.gitbranchingdemo;
 
-import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.view.View;
-
 import java.util.ArrayList;
 import java.util.List;
 import android.support.v7.app.AppCompatActivity;
 
-import static android.support.v7.recyclerview.R.styleable.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
     private RecyclerView mRecycler;
